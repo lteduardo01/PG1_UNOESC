5d25b7c51a3077fe41037fd3709c5e59efccdfe3
Eduardo <143460894+lteduardo01@users.noreply.github.com>
2024-03-12 16:47:54 -0300

    opeRador

--- Projetos/opeRador/Program.cs
+++ Projetos/opeRador/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿using System;
+
+class Tabuada
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um operador (+, -, *, /):");
+        string operador = Console.ReadLine();
+
+        Console.WriteLine("Digite um número (1-9):");
+        float numero = float.Parse(Console.ReadLine());
+
+        for (float i = 1; i <= 9; i++)
+        {
+            float resultado = 0;
+
+            switch (operador)
+            {
+                case "+":
+                    resultado = numero + i;
+                    break;
+                case "-":
+                    resultado = numero - i;
+                    break;
+                case "*":
+                    resultado = numero * i;
+                    break;
+                case "/":
+                    resultado = numero / i;
+                    break;
+            }
+
+            Console.WriteLine("{0} {1} {2} = {3}", numero, operador, i, resultado.ToString("N2"));
+        }
+    }
+}
